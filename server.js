ecd45cc5eebcf7205915cd70ed6de280997438f8
Meetsimar <meetsimark@gmail.com>
2023-01-19 20:44:35 -0500

    commit

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+* BTI425 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students. *
+* Name: Meetsimar Kaur Student ID: 106510217 Date: 19/01/23
+* Cyclic Link: https://periwinkle-drill-gown.cyclic.app/
+* ********************************************************************************/
+
+
 const express = require('express');
 const path = require('path');
 var cors = require('cors')
